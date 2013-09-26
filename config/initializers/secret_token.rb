1f85c77fac71e2232622a7fc8ff25e1284ed06b2
Van Michael <van.michael.nguyen@gmail.com>
2013-09-26 11:15:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArcoPortfolio::Application.config.secret_token = 'a0b022264e21d370af809a78d0b1663cdb93f9dec5cbb2673a8e31c7e873617affe67689727b9060a7f564012797c8f9bb711a2a1688ffd562d0fc50b99b510c'
